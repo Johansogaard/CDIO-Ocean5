0dbd8ff6adf83342b441f04a114f6f16b309054b
mikkel <mtschwarz9@gmail.com>
2023-01-10 16:48:31 +0100

    ændret i tax, så der er description

--- src/main/java/gui_GameFields/GUI_Tax.java
+++ src/main/java/gui_GameFields/GUI_Tax.java
@@ -7,7 +7,7 @@ import java.awt.*;
 public class GUI_Tax extends GUI_Parentfield{
     public GUI_Tax(String title, String price,String description)
     {
-        super(Color.gray,Color.black,title,price,description);
+        super(Color.gray,Color.black,"Tax",price,"Betal skat");
 
     }
 
