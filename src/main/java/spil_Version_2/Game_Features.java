8a2008a39736ca61a818d0301bc0ceb442d4593c
Johansogaard <Johansogaardjorgensen@gmail.com>
2023-01-10 16:02:05 +0100

    Nu virker chancekort

--- src/main/java/spil_Version_2/Game_Features.java
+++ src/main/java/spil_Version_2/Game_Features.java
@@ -22,11 +22,58 @@ public class Game_Features {
         }
         public static ArrayList<Parent_Card> cards()
         {
-
+//tilføjer kort til array, og gør rækkefølgen ranom
             ArrayList<Parent_Card> cardArray = new ArrayList<Parent_Card>();
             ArrayList<Parent_Card> randomCardArray = new ArrayList<Parent_Card>();
-            cardArray.add(new RykTilStart_Card());
-            cardArray.add(new RykFelterFrem_Card());
+            cardArray.add(new Matador_Legat(40000, true, "Tillykke, du har vundet Matador-Legatet; 40000kr!"));
+
+            //chancekort
+            // ryk til start, uden gevinst
+            cardArray.add(new RykTilStart_Card(0));
+            cardArray.add(new RykTilStart_Card(0));
+
+            //ryk frem eller tilbage
+            int[] spacesToMove = {3, -3,-3};
+            for (int space : spacesToMove) {
+                cardArray.add(new RykFelter_Card(space));
+            }
+            //modtag eller betal: false=betal, true=modtag
+            cardArray.add(new Straf_Eller_Gevinst(200, false, "Betal kr 200 for levering af 2 kasser øl"));
+            cardArray.add(new Straf_Eller_Gevinst(3000,false , "Betal 3000 for reparation af deres vogn"));
+            cardArray.add(new Straf_Eller_Gevinst(3000,false , "Betal 3000 for reparation af deres vogn"));
+            cardArray.add(new Straf_Eller_Gevinst(1000,false , "De har købt 4 nye dæk til Deres vogn, betal kr 1000"));
+            cardArray.add(new Straf_Eller_Gevinst(200,false , "De har fået en parkeringsbøde, betal kr 200 i bøde"));
+            cardArray.add(new Straf_Eller_Gevinst(1000,false , "Betal deres bilforsikring, kr 1000"));
+            cardArray.add(new Straf_Eller_Gevinst(200  ,false , "De har været udenlands og købt for mange smøger, betal kr 200 i told."));
+            cardArray.add(new Straf_Eller_Gevinst(2000,false , "Tandlægeregning, betal kr 2000."));
+            cardArray.add(new Straf_Eller_Gevinst(500,true , "De har vundet i klasselotteriet. Modtag 500 kr."));
+            cardArray.add(new Straf_Eller_Gevinst(500,true , "De har vundet i klasselotteriet. Modtag 500 kr."));
+            cardArray.add(new Straf_Eller_Gevinst(1000,true , "De modtager Deres aktieudbytte. Modtag kr 1000 af banken"));
+            cardArray.add(new Straf_Eller_Gevinst(1000,true , "De modtager Deres aktieudbytte. Modtag kr 1000 af banken"));
+            cardArray.add(new Straf_Eller_Gevinst(1000,true , "De modtager Deres aktieudbytte. Modtag kr 1000 af banken"));
+            cardArray.add(new Straf_Eller_Gevinst(3000,true , "Kommunen har eftergivet et kvartals skat. Hæv i banken 3000 kr."));
+            cardArray.add(new Straf_Eller_Gevinst(1000,true , "De have en række med elleve rigtige i tipning, modtag kr 1000"));
+            cardArray.add(new Straf_Eller_Gevinst(1000,true , "Grundet dyrtiden har De fået gageforhøjelse, modtag kr 1000."));
+            cardArray.add(new Straf_Eller_Gevinst(1000,true , "Deres præmieobligation er udtrykket. De modtager 1000 kr af banken."));
+
+            cardArray.add(new Straf_Eller_Gevinst(1000,true , "Deres præmieobligation er udtrykket. De modtager 1000 kr af banken."));
+            cardArray.add(new Straf_Eller_Gevinst(1000,true , "De har solg nogle gamle møbler på auktion. Modtag 1000 kr af banken."));
+            cardArray.add(new Straf_Eller_Gevinst(200,true , "Værdien af egen avl fra nyttehaven udgør 200 som de modtager af banken"));
+            cardArray.add(new Straf_Eller_Gevinst(300,false , "Betal for vognvask og smøring kr 300"));
+            cardArray.add(new Straf_Eller_Gevinst(1000,false , "De har kørt frem for “fuldt stop”, Betal 1000 kroner i bøde"));
+
+            cardArray.add(new MovePlayerCard( 11,"Ryk frem til Frederiksberg Allé. Hvis De passere START, indkasser da 4000 kr.\n"));
+            cardArray.add(new MovePlayerCard(15 ,"Tag med Mols-Linien, flyt brikken frem og hvis De passerer START indkassér da kr 4000.\n"));
+            cardArray.add(new MovePlayerCard( 25,"Ryk frem til Grønningen, hvis De passerer start indkasser da kr 4000\n"));
+            cardArray.add(new MovePlayerCard( 32,"Ryk frem til Vimmelskaftet, hvis de passerer start indkasser da kr 4000\n"));
+            cardArray.add(new MovePlayerCard( 19,"Ryk frem til Strandvejen. Hvis De passere START, indkasser da 4000 kr.\n"));
+
+
+
+
+
+
+
             Random rand = new Random();
             while (true) {
                 int random;
