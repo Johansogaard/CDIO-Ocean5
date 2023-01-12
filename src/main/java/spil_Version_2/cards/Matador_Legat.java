70782db54ba97eab1a826fdc1dcdc5373ca1fd0c
Anders Jefsen <andersjeff397@gmail.com>
2023-01-12 12:24:24 +0100

    nu virker moveplayer når man rykker tilbage <3 gangshit

--- src/main/java/spil_Version_2/cards/Matador_Legat.java
+++ src/main/java/spil_Version_2/cards/Matador_Legat.java
@@ -18,7 +18,7 @@ public class Matador_Legat extends Parent_Card {
             player.showchancecard("Tillykke, du har vundet Matador-Legatet; 40000kr!");
             player.updatePlayerBalance(40000);
         } else {
-            player.showchancecard("Desværre, du har for mange moneys, så du får ikke et Legat");
+            player.showchancecard("Du har vundet Matador-legatet, men desværre har du for mange moneys, så du får ikke et Legat #velfærdsstaten");
         }
     }
 }
